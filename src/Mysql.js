b2e615a5c6d773a01bf8b353fbc65b4fbf2ed560
Vincent Chan <fswit@163.com>
2017-11-09 15:45:27 +0800

    init proj

---
+++ src/Mysql.js
@@ -0,0 +1,50 @@
+/*************************************************
+ * @copyright 2017 Flision Corporation Inc.
+ * @author: Vincent Chan @ Canton
+ * @date: 2017年11月09日
+ * @version: 1.0.0
+ * @description:
+ **************************************************/
+
+class Mysql {
+    constructor( options ){
+
+    }
+
+    on(event_name, callback){
+
+    }
+
+    begin(){
+
+    }
+
+    table( name ){
+
+    }
+
+    view( name ){
+
+    }
+
+    proc( name ){
+
+    }
+
+    exec( sql, ...args ){
+
+    }
+
+    ping(){
+
+    }
+
+    release(){
+
+    }
+}
+
+Mysql.Events = require('./events');
+
+
+module.exports = Mysql;
---
+++ src/events.js
@@ -0,0 +1,12 @@
+/*************************************************
+ * @copyright 2017 Flision Corporation Inc.
+ * @author: Vincent Chan @ Canton
+ * @date: 2017年11月09日
+ * @version: 1.0.0
+ * @description:
+ **************************************************/
+
+module.exports = {
+    ERROR: 'error',
+    SQL:'sql'
+};
